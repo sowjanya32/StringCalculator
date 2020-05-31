89852951f9b0c66c2f7d7b6ca247d493e7653e93
Sowjanya <Sowjanya@Chaitu-PC>
2020-05-31 20:16:06 +0530

    Added condition for negative number in Add method

--- StringCalculator/src/com/string/calculator/main/StringCalculator.java
+++ StringCalculator/src/com/string/calculator/main/StringCalculator.java
@@ -2,14 +2,19 @@ package com.string.calculator.main;
 
 public class StringCalculator {
 
-	public int Add(String input) {
-		int sum = 0;
+	public int Add(String input)   {
+		int sum = 0, number = 0;
 		if(input.length() < 1)
 			return sum;
 		String[] splitString = input.split("[,\\n//;]+");
 		for(String str : splitString) {
-			if(str.length() > 0)
-				sum = sum + Integer.parseInt(str);
+			if(str.length() > 0) {
+				number = Integer.parseInt(str);
+				if(number < 0) {
+					throw new IllegalArgumentException("negatives not allowed and the negative number is : " + number);
+				}
+			}
+			sum = sum + number;
 		}
 		return sum;
 	}
