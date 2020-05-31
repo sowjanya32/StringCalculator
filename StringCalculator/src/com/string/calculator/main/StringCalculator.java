9747b7ce83cff42f74376e4898e5241f679ffa8b
Sowjanya <Sowjanya@Chaitu-PC>
2020-05-31 15:29:43 +0530

    Added functionality in Add method returns the sum of the numbers

--- StringCalculator/src/com/string/calculator/main/StringCalculator.java
+++ StringCalculator/src/com/string/calculator/main/StringCalculator.java
@@ -4,9 +4,13 @@ public class StringCalculator {
 
 	public int Add(String input) {
 		int sum = 0;
-		if(input.length() > 1)
+		if(input.length() < 1)
 			return sum;
-		return 0;
+		String[] splitString = input.split(",");
+		for(String str : splitString) {
+			sum = sum + Integer.parseInt(str);
+		}
+		return sum;
 	}
 
 }
