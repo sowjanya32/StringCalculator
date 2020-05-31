fe62628593233ddb255422f163825e151009c587
Sowjanya <Sowjanya@Chaitu-PC>
2020-05-31 15:34:32 +0530

    Refactored the tests

--- StringCalculator/src/com/string/calculator/test/StringCalculatorTest.java
+++ StringCalculator/src/com/string/calculator/test/StringCalculatorTest.java
@@ -7,26 +7,22 @@ import org.junit.jupiter.api.Test;
 import com.string.calculator.main.StringCalculator;
 
 class StringCalculatorTest {
+	
+	StringCalculator stringCalculator = new StringCalculator();
 
 	@Test
 	public void testEmptyString() {
-		StringCalculator stringCalculator = new StringCalculator();
-		int result = stringCalculator.Add("");
-		assertEquals(0, result);
+		assertEquals(0, stringCalculator.Add(""));
 	}
 	
 	@Test
 	public void testOneNumber() {
-		StringCalculator stringCalculator = new StringCalculator();
-		int result = stringCalculator.Add("1");
-		assertEquals(1, result);
+		assertEquals(1, stringCalculator.Add("1"));
 	}
 	
 	@Test
 	public void testTwoNumber() {
-		StringCalculator stringCalculator = new StringCalculator();
-		int result = stringCalculator.Add("1,5");
-		assertEquals(6, result);
+		assertEquals(6, stringCalculator.Add("1,5"));
 	}
 
 }
